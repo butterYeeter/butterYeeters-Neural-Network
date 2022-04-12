ef18a292f4eaabdf8543a9c46296052f10fb5f99
butterYeeter <100842250+butterYeeter@users.noreply.github.com>
2022-04-12 12:20:58 +0200

    Add files via upload

---
+++ Dense.java
@@ -0,0 +1,51 @@
+
+public class Dense {
+	double[][] weights, biases, output;
+	
+	Dense(int inputs, int neurons)
+	{
+		weights = new double[inputs][neurons];
+		biases = new double[1][neurons];
+		
+		for(int i = 0; i  < inputs; i++)
+		{
+			for(int j = 0; j < neurons; j++)
+			{
+				weights[i][j] = Math.random();
+			}
+		}
+		
+		for(int i = 0; i  < 1; i++)
+		{
+			for(int j = 0; j < neurons; j++)
+			{
+				biases[i][j] = 2;
+			}
+		}
+	}
+	
+	public void forward(double[][] weights, double[][] inputs)
+	{
+		double[][] o = new double[inputs.length][weights[0].length];
+		for(int i = 0; i < inputs.length; i++)
+		{
+			for(int j = 0; j < weights[0].length; j++)
+			{
+				o[i][j] = 0;
+				for(int k = 0; k < weights.length; k++)
+				{
+					o[i][j] += weights[k][j] * inputs[i][k];
+				}
+			}
+		}
+		
+		for(int i = 0; i < 1; i++)
+		{
+			for(int j = 0; j < o[0].length; j++)
+			{
+				o[i][j] = o[i][j] + biases[i][j];
+			}
+		}
+		output = o;
+	}
+}
---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main {
+	public static void main(String args[])
+	{
+		double[][] inputs = {{1.0,1.38, 0.201, 1.098}};
+		Dense l1 = new Dense(4, 5);
+		Dense l2 = new Dense(5, 2);
+		
+		l1.forward(l1.weights, inputs);
+		//l2.forward(l2.weights, l1.output);
+		
+		for(int i = 0; i < 1; i++)
+		{
+			for(int j = 0; j < l1.output[0].length; j++)
+			{
+				System.out.printf("%.2f\t", l1.output[i][j]);
+			}
+		}
+		
+		System.out.print("\n");
+		for(int i = 0; i < l1.weights.length; i++)
+		{
+			for(int j = 0; j < l1.weights[0].length; j++)
+			{
+				System.out.printf("%.2f\t", l1.weights[i][j]);
+			}
+		}
+	}
+}
