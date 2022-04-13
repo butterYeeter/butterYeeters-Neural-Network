ac19d169efc3a27c48f6f171c0e22ec0adf48708
butterYeeter <100842250+butterYeeter@users.noreply.github.com>
2022-04-13 15:10:19 +0200

    Added activation functions
    ReLU Activation Function added. Softmax Activation Function added for classification problems;

--- Main.java
+++ Main.java
@@ -1,18 +1,28 @@
 public class Main {
 	public static void main(String args[])
 	{
-		double[][] inputs = {{1.0,1.38, 0.201, 1.098}};
-		Dense l1 = new Dense(4, 5);
-		Dense l2 = new Dense(5, 2);
+		double[][] inputs = {{2, 4, 5, 1}};
+		Dense l1 = new Dense(4, 2);
+		//Dense l2 = new Dense(5, 2);
 		
-		l1.forward(l1.weights, inputs);
+		l1.forward(inputs, l1.weights);
+		Activation a1 = new Activation();
+		a1.softmax(l1.output);
+		double[][] jb = a1.getSoftMax();
+		for(int i = 0; i < jb.length; i++)
+		{
+			for(int j = 0; j < jb[0].length; j++)
+			{
+				System.out.printf("%.2f\t", jb[i][j]);
+			}
+		}
 		//l2.forward(l2.weights, l1.output);
 		//Test
 		for(int i = 0; i < 1; i++)
 		{
 			for(int j = 0; j < l1.output[0].length; j++)
 			{
-				System.out.printf("%.2f\t", l1.output[i][j]);
+				System.out.printf("%.1f\t", l1.output[i][j]);
 			}
 		}
 		
@@ -21,7 +31,7 @@ public class Main {
 		{
 			for(int j = 0; j < l1.weights[0].length; j++)
 			{
-				System.out.printf("%.2f\t", l1.weights[i][j]);
+				System.out.printf("%.1f\t", l1.weights[i][j]);
 			}
 		}
 	}
